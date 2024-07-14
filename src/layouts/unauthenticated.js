f5aa8cb3c0d7426cfcb5cd119e5d022c7b9d6438
ayo <olasupoayomide557@gmail.com>
2024-07-14 16:18:07 +0100

    seen

--- src/layouts/unauthenticated.js
+++ src/layouts/unauthenticated.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router";
-import imagePath from "../images/path.png";
+import imagePath from "../images/path.PNG";
 
 export default function Unauthenticated() {
   return (
